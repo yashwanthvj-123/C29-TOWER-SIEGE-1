7d888d63916c24bc6fa19c37095ecd3da1220b17
yashwanthvj-123 <73013758+yashwanthvj-123@users.noreply.github.com>
2021-02-23 20:06:37 +0530

    Add files via upload

---
+++ BlockIV.js
@@ -0,0 +1,24 @@
+
+class BlockIV {
+    
+    constructor (x,y) {
+
+        var options= {restitution:0.8 , friction:1.0 , density:1.0 }
+
+        this.body=Bodies.rectangle (x,y,30,30,options);
+        this.width=30;
+        this.height=30;
+        World.add (world,this.body);
+
+    }
+
+    display () {
+
+        var pos=this.body.position;
+        rectMode (CENTER);
+        fill ("pink");
+        rect (pos.x,pos.y,this.width,this.height);
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# C29-TOWER-SIEGE-1
+# TowerSiege-1
